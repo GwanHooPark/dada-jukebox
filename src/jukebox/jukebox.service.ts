25347e08378aa31910770df459d3b88988ff7dde
박관후 <ghpark@daou.co.kr>
2022-04-07 16:24:57 +0900

    enum 처리 및 리팩토링

--- src/jukebox/jukebox.service.ts
+++ src/jukebox/jukebox.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger  } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import axios from 'axios';
 import cheerio, { load } from 'cheerio';
@@ -8,11 +8,18 @@ type MusicInfo = {
     artist : string
 }
 
+export enum MBC {
+    homeUrl = 'https://miniweb.imbc.com',
+    selectMusicPageUrl = 'https://miniweb.imbc.com/Music?page=1&progCode=FM4U000001070',
+    listSelector = 'table.list-type tbody tr'
+}
+
 @Injectable()
 export class JukeboxService {
 
     private readonly logger = new Logger(JukeboxService.name);
 
+    //@Cron('10 10 13 * * *', { name: 'get Music' })
     @Cron('10 * * * * *', { name: 'get Music' })
     musicListSchedule(): void {
         this.logger.log('get music....');
@@ -21,11 +28,11 @@ export class JukeboxService {
         });
     }
 
-    async getMusicData(): Promise<Array<MusicInfo>> {
-
-        const html:any = await this.getHtml();
+    async getMusicData(): Promise<Array<MusicInfo>> {        
+        const todayUrl:string = await this.getUrl();
+        const html:any = await this.getHtml(`${MBC.homeUrl}${todayUrl}`);
         const $:CheerioRoot = cheerio.load(html.data);
-        const list:any = $('table.list-type tbody tr');
+        const list:any = $(MBC.listSelector);
         
         let musicInfos:Array<MusicInfo> = [];
         list.each((idx, el) => {
@@ -36,10 +43,22 @@ export class JukeboxService {
         }); 
         return musicInfos;
     }
-    
-    async getHtml(): Promise<string> {
+
+    async getUrl(): Promise<string> {
         try {
-            return axios.get('https://miniweb.imbc.com/Music/View?seqID=5879&progCode=FM4U000001070&page=1');
+            let html:any = await this.getHtml(MBC.selectMusicPageUrl);
+            const $:CheerioRoot = cheerio.load(html.data);
+            const lettt = $(MBC.listSelector).first().find('a').attr('href');
+            return lettt;
+        }catch(err){
+            console.log(err);
+        }
+
+    }
+
+    async getHtml(url:string): Promise<string> {
+        try {
+            return axios.get(url);
         }catch(err) {
             this.logger.log(err);
         }
