9e45610f6eced3c71c222ecc14110c313a3db9ad
박관후 <ghpark@daou.co.kr>
2022-04-27 13:17:12 +0900

    hbs add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+
+  private readonly logger = new Logger(AppController.name);
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @Render('index')
   root() {
-    return {message : 'hello dada jukebox'};
+    console.log('index page!!!');
+    this.logger.log("testing page");
+    return {message : `hello dada jukebox ${new Date()}`};
   }
 }
